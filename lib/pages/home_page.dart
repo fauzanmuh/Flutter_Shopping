06979f1c6716513ca2694fc2093cc11714fe35d4
fauzanmuh <f4uz4n.oioi@gmail.com>
2021-11-08 17:30:13 +0700

    Switch Pages

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,9 +1,49 @@
+import 'package:flutter_shopping/models/item.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
+  HomePage({Key? key}) : super(key: key);
+  final List<Item> items = [
+    Item(name: 'Sugar', price: 5000),
+    Item(name: 'Salt', price: 2000)
+  ];
 
   @override
   Widget build(BuildContext context) {
-    throw UnimplementedError();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Shopping List'),
+      ),
+    body: Container(
+        margin: const EdgeInsets.all(8),
+        child: ListView.builder(
+          padding: const EdgeInsets.all(8),
+          itemCount: items.length,
+          itemBuilder: (context, index) {
+            final item = items[index];
+            return InkWell(
+              onTap: () {
+                Navigator.pushNamed(context, '/item');
+              },
+              child: Card(
+                child: Container(
+                  margin: const EdgeInsets.all(8),
+                  child: Row(
+                    children: [
+                      Expanded(child: Text(item.name)),
+                      Expanded(
+                          child: Text(
+                        item.price.toString(),
+                        textAlign: TextAlign.end,
+                      ))
+                    ],
+                  ),
+                ),
+              ),
+            );
+          },
+        ),
+      ),
+    );
   }
 }
--- lib/pages/item_page.dart
+++ lib/pages/item_page.dart
@@ -1,9 +1,16 @@
 import 'package:flutter/material.dart';
 
 class ItemPage extends StatelessWidget {
+  const ItemPage({Key? key}) : super(key: key);
+
 
   @override
   Widget build(BuildContext context) {
-    throw UnimplementedError();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Shopping List'),
+      ),
+      body: Container(),
+    );
   }
 }
